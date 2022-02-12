d639e3bc7bb8896e03bfc01fde9ced158c6ea170
Omanpreet Kaur <kaur0026@rangers.uwp.edu>
2022-02-11 21:23:29 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Omanpreet Kaur on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
